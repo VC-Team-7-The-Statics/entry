604a204b57cd4896cd5009d32389ddd42d80946b
Intaek H <ahiou1ahiou@gmail.com>
2022-06-07 16:45:49 +0900

    feat: 자동 로그인과 일반 로그인 기능 구현

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,7 @@
-import { useNavigate } from "react-router-dom";
-
 function HomePage() {
-  const navigate = useNavigate();
-
-  const sendToken = () => {
-    if (window.isNativeApp) {
-      window.ReactNativeWebView.postMessage("token: some-token??");
-    }
-  };
-
   return (
     <div>
-      <h1>home page screens</h1>
-      {window.isNativeApp && <h3>this is on WebView</h3>}
-      <button onClick={() => navigate("/welcome")}>To Welcome Page</button>
-      <button onClick={sendToken}>SEND TOKEN TO NATIVE</button>
+      <h1>홈 페이지 입니다.</h1>
     </div>
   );
 }
